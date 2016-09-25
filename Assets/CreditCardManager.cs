38a067a1819bb8b6a905c2bcba82ef94887c0c8b
calder <calderarchinuk@gmail.com>
2016-09-24 17:10:30 -0700

    finished credit card screen added map style added map icons added player particle

--- Assets/CreditCardManager.cs
+++ Assets/CreditCardManager.cs
@@ -3,13 +3,8 @@ using System.Collections;
 
 public class CreditCardManager : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+	public void GoToShop()
+	{
+		MasterManager.Instance.ChangeScene("shop");
 	}
 }
--- Assets/MapManager.cs
+++ Assets/MapManager.cs
@@ -22,6 +22,17 @@ public class MapManager : MonoBehaviour
 
 	List<string> markers = new List<string>
 	{
+		//"&markers=icon:http://i.imgur.com/7jbGAnD.png%7C%7C49.2890615,-123.1412927",
+
+		"&markers=icon:http://i.imgur.com/7jbGAnD.png%7C%7C49.282428,-123.1412927",
+		"&markers=icon:http://i.imgur.com/7jbGAnD.png%7C%7C49.281148,-123.119334",
+		"&markers=icon:http://i.imgur.com/fOhHSpr.png%7C%7C49.283039,-123.115147",
+		"&markers=icon:http://i.imgur.com/7jbGAnD.png%7C%7C49.278847,-123.116423",
+		"&markers=icon:http://i.imgur.com/7jbGAnD.png%7C%7C49.279406,-123.122609",
+		"&markers=icon:http://i.imgur.com/7jbGAnD.png%7C%7C49.276736,-123.121911",
+		"&markers=icon:http://i.imgur.com/7jbGAnD.png%7C%7C49.276006,-123.127235"
+
+		/*
 		"&markers=color:red%7Clabel:C%7C49.2890615,-123.1412927",
 		"&markers=color:red%7Clabel:C%7C49.281148,-123.119334",
 		"&markers=color:red%7Clabel:C%7C49.283039,-123.115147",
@@ -29,6 +40,7 @@ public class MapManager : MonoBehaviour
 		"&markers=color:red%7Clabel:C%7C49.279406,-123.122609",
 		"&markers=color:red%7Clabel:C%7C49.276736,-123.121911",
 		"&markers=color:red%7Clabel:C%7C49.276006,-123.127235"
+		*/
 	};
 
 	void Start ()
@@ -54,23 +66,26 @@ public class MapManager : MonoBehaviour
 		}
 	}
 
-	
+	string key = "AIzaSyA3PCTWtIVqJkbp41RlYFsrba3eCkY8aTA";
 	LocationInfo li;
 	Vector2 size = new Vector2(2048,2048);
 
 	public IEnumerator RequestMap(float lat, float lon, float zoom)
 	{
-
+		//lock this down!
 		string url = "";
 
-		url = "https://maps.googleapis.com/maps/api/staticmap?center="+lat+","+lon+"&zoom="+zoom+"&size="+size.x+"x"+size.y;
+		url = "https://maps.googleapis.com/maps/api/staticmap?key="+key+"&center="+lat+","+lon+"&zoom="+zoom+"&format=png&maptype=roadmap&style=feature:administrative%7Celement:geometry.fill%7Csaturation:28%7Clightness:-34&style=feature:administrative%7Celement:labels.icon%7Cvisibility:off&style=feature:administrative%7Celement:labels.text%7Cvisibility:on&style=feature:landscape%7Celement:geometry.fill%7Csaturation:-61%7Clightness:28%7Cgamma:1.09&style=feature:poi%7Celement:geometry.fill%7Csaturation:-39%7Clightness:-21%7Cgamma:1.76&style=feature:poi%7Celement:labels.icon%7Cvisibility:off&style=feature:poi%7Celement:labels.text%7Cvisibility:off&style=feature:road%7Celement:geometry.stroke%7Csaturation:40%7Clightness:-24%7Cgamma:8.24&style=feature:road%7Celement:labels.icon%7Cvisibility:off&style=feature:road%7Celement:labels.text%7Cvisibility:off&style=feature:transit%7Celement:geometry.fill%7Csaturation:73&style=feature:transit%7Celement:labels.icon%7Cvisibility:off&style=feature:transit%7Celement:labels.text%7Cvisibility:off&style=feature:water%7Celement:geometry.fill%7Csaturation:29%7Clightness:-6&size=2048x2048";
+
+
+		//url = "https://maps.googleapis.com/maps/api/staticmap?center="+lat+","+lon+"&zoom="+zoom+"&size="+size.x+"x"+size.y;
 
 		foreach (var m in markers)
 		{
 			url += m;
 		}
 
-		url += "&key="+"AIzaSyA3PCTWtIVqJkbp41RlYFsrba3eCkY8aTA";
+		//url += "&key="+"AIzaSyA3PCTWtIVqJkbp41RlYFsrba3eCkY8aTA";
 
 		Debug.Log(url);
 
