a5912d12bed35893cd487c04ec4c03215975fa61
calder <calderarchinuk@gmail.com>
2016-09-25 11:48:27 -0700

    adde pool system to mastermanager (in case...) added logic for laser count image to gamescreen

--- Assets/MasterManager.cs
+++ Assets/MasterManager.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using System.Collections.Generic;
 
 // once at game start
 public class MasterManager : MonoBehaviour
@@ -14,6 +15,17 @@ public class MasterManager : MonoBehaviour
 	public AudioClip ChestExplode;
 	public AudioClip ItemPickup;
 
+
+	//public List<GameObject>ParticlePool = new List<GameObject>();
+	public Dictionary<string,List<GameObject>>Pools = new Dictionary<string, List<GameObject>>();
+
+	public GameObject GemPrefab;
+	public GameObject LaserPrefab;
+	public GameObject CashPrefab;
+	public GameObject ExplodeParticle;
+	public GameObject ChestPrefab;
+	public GameObject SuperChestPrefab;
+
 	void Start ()
 	{
 		if (Instance != null)
@@ -23,7 +35,81 @@ public class MasterManager : MonoBehaviour
 		}
 		DontDestroyOnLoad(gameObject);
 		Instance = this;
+
+		Pools.Add("chest",new List<GameObject>());
+		Pools.Add("superchest",new List<GameObject>());
+		Pools.Add("gem",new List<GameObject>());
+		Pools.Add("laser",new List<GameObject>());
+		Pools.Add("cash",new List<GameObject>());
+		Pools.Add("explode",new List<GameObject>());
+		for (int i = 0; i<5; i++)
+		{
+			Pools["chest"].Add((GameObject)GameObject.Instantiate(ChestPrefab));
+			Pools["superchest"].Add((GameObject)GameObject.Instantiate(SuperChestPrefab));
+			Pools["gem"].Add((GameObject)GameObject.Instantiate(GemPrefab));
+			Pools["laser"].Add((GameObject)GameObject.Instantiate(Laser));
+			Pools["cash"].Add((GameObject)GameObject.Instantiate(CashPrefab));
+			Pools["explode"].Add((GameObject)GameObject.Instantiate(ExplodeParticle));
+		}
 	}
+
+	public GameObject GetObject(string name, float duration = -1)
+	{
+		GameObject returngo;
+		if (Pools[name].Count == 0)
+		{
+			//create new
+			switch (name)
+			{
+			case "chest":
+			Pools["chest"].Add((GameObject)GameObject.Instantiate(ChestPrefab));
+				break;
+			case "superchest":
+				Pools["superchest"].Add((GameObject)GameObject.Instantiate(SuperChestPrefab));
+				break;
+			case "gem":
+				Pools["gem"].Add((GameObject)GameObject.Instantiate(GemPrefab));
+				break;
+			case "laser":
+				Pools["laser"].Add((GameObject)GameObject.Instantiate(Laser));
+				break;
+			case "cash":
+				Pools["cash"].Add((GameObject)GameObject.Instantiate(CashPrefab));
+				break;
+			case "explode":
+				Pools["explode"].Add((GameObject)GameObject.Instantiate(ExplodeParticle));
+				break;
+			}
+		}
+
+		returngo = Pools[name][0];
+
+		if (duration > 0)
+		{
+			StartCoroutine(DelayReturn(name,returngo,duration));
+		}
+
+		return returngo;
+	}
+
+	IEnumerator DelayReturn(string name, GameObject go, float duration)
+	{
+		yield return new WaitForSeconds(duration);
+		ReturnToPool(name,go);
+
+	}
+
+	public void ReturnToPool(string name, GameObject go)
+	{
+		if (go != null)
+			Pools[name].Add(go);
+	}
+
+	public void DoExplode(Vector3 pos)
+	{
+		GameObject.Instantiate(ExplodeParticle,pos,Quaternion.identity);
+	}
+
 	public void ChangeScene(string sceneName)
 	{
 		SceneManager.LoadScene(sceneName);
